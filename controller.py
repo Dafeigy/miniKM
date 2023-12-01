5e905e388a4052bcc6daa352028c04f466a3ac80
Dafeigy <1070642765@qq.com>
2023-12-01 17:14:37 +0800

    <update>: TEST with SFC

--- controller.py
+++ controller.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     import comm
     import pickle
     vk_keys = list(vk.keys())
-    target_ip = "192.168.0.19"
-    sender = comm.UDPserver(ip_addr="192.168.0.124", port=1224)
+    target_ip = "192.168.0.124"
+    sender = comm.UDPserver(ip_addr="192.168.0.19", port=1224)
     while True:
         # 发送flags还是res？
         # 取决于场景，如果是通用场景自然是flags
@@ -24,4 +24,4 @@ if __name__ == "__main__":
         data = str(res).encode("utf-8")
         sender.send_data(target_ip, data)
         time.sleep(0.05)
-        # print("发送给客户端 %s 的数据: %s\n" % (target_ip, data))
+        print("发送给客户端 %s 的数据: %s\n" % (target_ip, data))
