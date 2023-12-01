a4ed747f51f1bdd969e276485fd4a4303618df40
Dafeigy <1070642765@qq.com>
2023-12-01 16:32:11 +0800

    <Update>: Finished.

--- controller.py
+++ controller.py
@@ -1,15 +1,16 @@
 import win32api
 import win32con
 from config import vk
+import time
 
 
 if __name__ == "__main__":
     import time
-    import utils
+    import comm
     import pickle
     vk_keys = list(vk.keys())
-    target_ip = "192.168.0.124"
-    sender = utils.UDPserver(ip_addr="192.168.0.19", port=1224)
+    target_ip = "192.168.0.19"
+    sender = comm.UDPserver(ip_addr="192.168.0.124", port=1224)
     while True:
         # 发送flags还是res？
         # 取决于场景，如果是通用场景自然是flags
@@ -22,4 +23,5 @@ if __name__ == "__main__":
         # data = pickle.dumps(flags)
         data = str(res).encode("utf-8")
         sender.send_data(target_ip, data)
-        print("发送给客户端 %s 的数据: %s\n" % (target_ip, data))
+        time.sleep(0.05)
+        # print("发送给客户端 %s 的数据: %s\n" % (target_ip, data))
