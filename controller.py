ff8c21c84ff75de1d9d0644e14d7b4d8632edd86
Dafeigy <1070642765@qq.com>
2023-12-01 15:10:12 +0800

    <Update>: Controller

--- controller.py
+++ controller.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     import utils
     import pickle
     vk_keys = list(vk.keys())
-    sender = utils.UDPserver(ip_addr="127.0.0.1", port=1224)
+    target_ip = "192.168.0.124"
+    sender = utils.UDPserver(ip_addr="192.168.0.19", port=1224)
     while True:
         # 发送flags还是res？
         # 取决于场景，如果是通用场景自然是flags
@@ -20,9 +21,5 @@ if __name__ == "__main__":
         res = [i for i, e in enumerate(flags) if e < 0]
         # data = pickle.dumps(flags)
         data = str(res).encode("utf-8")
-        sender.send_data("127.0.0.1", data)
-        print("发送给客户端 %s 的数据: %s\n" % ("127.0.0.1", data))
-        # client_socket.close()
-        # 
-        # for each in res:
-        #     print(f"Now {vk_keys[each]} is pressed.")
+        sender.send_data(target_ip, data)
+        print("发送给客户端 %s 的数据: %s\n" % (target_ip, data))
