048be3831aff9445b268aa1bd491c04b02378dff
Dafeigy <1070642765@qq.com>
2023-12-04 15:58:03 +0800

    <Update>: Update data form transform

--- FT.py
+++ FT.py
@@ -7,14 +7,22 @@ from samples import sample
 def process_complex(data:dict)->list:
     return [abs(complex(each['real'],each['image'])) for each in data]
 
-if __name__ == "__main__":
-    # 修改`config.py`中的配置内容。
+def rec_trans(sample):
     temp = tuple(
     torch.tensor(
         process_complex(sample['eSTIMATION'][i]['lSSRS'])) for i in range(nb_gNB_TX * nb_UE_Ports))
-
     tensor = torch.stack(temp, dim=1)  # 组合为一个张量
-    tensor = tensor.view(1, nb_subcarrier, 1, nb_gNB_TX * nb_UE_Ports)  # 调整尺寸为[624, 1, 2]
+    tensor = tensor.view(1, nb_subcarrier, 1, nb_gNB_TX * nb_UE_Ports)
+    return tensor
+
+def out_trans(output):
+    # TODO: convert output tensor[1*n] to the ideal output and like [best_idx].
+    pass
+
+if __name__ == "__main__":
+    # 修改`config.py`中的配置内容。
+    
+    tensor = rec_trans(sample)
 
     print(tensor.shape)
 
